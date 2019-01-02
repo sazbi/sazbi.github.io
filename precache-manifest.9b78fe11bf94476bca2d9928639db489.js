03e182db943d4b505ca8c5c3abcdcca4d4c47be5
sazbi <pierson.a@hotmail.com>
2019-01-02 01:43:34 -0500

    Updates

---
+++ precache-manifest.9b78fe11bf94476bca2d9928639db489.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2345a5c4d363dd55b0d5904d0d0c9087",
+    "url": "/static/media/angular.2345a5c4.png"
+  },
+  {
+    "revision": "15c247a47f55a8598880",
+    "url": "/static/css/main.da48f2c0.chunk.css"
+  },
+  {
+    "revision": "d81c1fcc0302907a15db",
+    "url": "/static/js/1.d81c1fcc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/static/media/react.8d859602.png"
+  },
+  {
+    "revision": "87d994875c22d91270c9d27302a45fb7",
+    "url": "/static/media/bootstrap.87d99487.png"
+  },
+  {
+    "revision": "8b8800e803fc813dc80d0b20fbe2cb07",
+    "url": "/static/media/vue.8b8800e8.png"
+  },
+  {
+    "revision": "15c247a47f55a8598880",
+    "url": "/static/js/main.15c247a4.chunk.js"
+  },
+  {
+    "revision": "17d706e8ae4ef8d4b1066ff942dc4c49",
+    "url": "/static/media/ruby.17d706e8.png"
+  },
+  {
+    "revision": "746d6d97e60e2856d0a3c59eed557471",
+    "url": "/static/media/rails.746d6d97.png"
+  },
+  {
+    "revision": "e15cf39a6053c2ae4e74d50f7d107cc8",
+    "url": "/static/media/postgresql.e15cf39a.png"
+  },
+  {
+    "revision": "4de645e16e2d3f5b51cfbe1323f13c9b",
+    "url": "/static/media/webpack.4de645e1.png"
+  },
+  {
+    "revision": "6ded14c8a9e078f1d9c305186271078d",
+    "url": "/static/media/wordpress.6ded14c8.png"
+  },
+  {
+    "revision": "ae28941dc621b5a3955b363ec734461a",
+    "url": "/index.html"
+  }
+];
